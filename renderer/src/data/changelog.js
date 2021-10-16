85900f4213b2cc9c7ed88aa7a6dd2e7f56a1fb4c
Rachael <85811185+RachaelSmith2@users.noreply.github.com>
2021-10-16 13:54:44 +1100

    first commit

---
+++ renderer/src/data/changelog.js
@@ -0,0 +1,24 @@
+export default {
+    description: "Temporary Login Delay **ISSUE UV",
+    changes: [
+        {
+            title: "Bug Fixes",
+            type: "fixed",
+            items: [
+                "MacOS should now not glitch and bug the delaying in the screen refresh rate caused by a minor bug issue",
+                "Windows Users have reportedly reported a Bug involving Crashing Graphics Cards (Has been Fixed) temporary."
+            ]
+        },
+        {
+            title: "Power Users & Developers",
+            type: "improved",
+            items: [
+                "Plugin data retrieval will now return the correct values instead of `undefined` for falsey values.",
+                "Plugin data can now be set before being retrieved.",
+                "The `Patcher` in `BdApi` now has a `getPatchesByCaller` function which will return all the patches corresponding to a caller string.",
+                "Plugins that fail on initial load will no longer be forever in a broken state. Thanks, Strencher. (https://github.com/Strencher)",
+                "React DevTools should now work on Linux! Thanks, Qb. (https://github.com/QbDesu)"
+            ]
+        }
+    ]
+};
